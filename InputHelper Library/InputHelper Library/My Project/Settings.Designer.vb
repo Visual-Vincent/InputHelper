ad43c721b63e9f449996281248c88bee4f641451
Visual Vincent <github-vv@mydoomsite.com>
2017-07-31 21:57:05 +0200

    Project update.

--- InputHelper Library/InputHelper Library/My Project/Settings.Designer.vb
+++ InputHelper Library/InputHelper Library/My Project/Settings.Designer.vb
@@ -64,9 +64,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.InputHelper.My.MySettings
+        Friend ReadOnly Property Settings() As Global.InputHelperLibrary.My.MySettings
             Get
-                Return Global.InputHelper.My.MySettings.Default
+                Return Global.InputHelperLibrary.My.MySettings.Default
             End Get
         End Property
     End Module
