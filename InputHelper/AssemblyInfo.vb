6cc22c6ac019d3d9b980c27983e07c7535f0244e
Visual-Vincent <github-vv@mydoomsite.com>
2023-12-07 14:08:52 +0100

    Added SupportedOSPlatform attribute to assembly for .NET 5 and newer

--- InputHelper/AssemblyInfo.vb
+++ InputHelper/AssemblyInfo.vb
@@ -1,5 +1,9 @@
-Imports System.Reflection
+﻿Imports System.Reflection
 Imports System.Resources
+
+#If NET5_0_OR_GREATER Then
+Imports System.Runtime.Versioning
+#End If
 
 <Assembly: AssemblyTitle("InputHelper")>
 <Assembly: AssemblyDescription("A .NET friendly library for simulating mouse and keyboard input.")>
@@ -7,3 +11,7 @@ Imports System.Resources
 <Assembly: AssemblyCopyright("Copyright © Vincent Bengtsson 2016-2023")>
 <Assembly: AssemblyVersion("2.1.0.1")>
 <Assembly: NeutralResourcesLanguage("en")>
+
+#If NET5_0_OR_GREATER Then
+<Assembly: SupportedOSPlatform("windows")>
+#End If
